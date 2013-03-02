2ffde1c222e2e4f2a28d2cbad8e50c5b911d5e91
Terence Chow <terence.chow.1986@gmail.com>
2013-03-02 17:28:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VolatilityApp::Application.config.secret_token = '20d27d70edc3cf72c561de7effb7fb720d38b1502faddc6546eea0f4f07c0a2638c122f898f4100e1f151a2c8774c7d4cae2e159419b5346e010ba58a3c6772b'
