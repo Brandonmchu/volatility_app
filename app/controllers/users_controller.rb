a315a0a0be2217cfcfe0f050acb9163c6695ac33
Brandon Chu <brandon@tunezy.com>
2013-03-16 19:11:46 -0400

    small refactor on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_filter :signed_in_user, :only => [:edit, :update]
-	before_filter :correct_user,   :only => [:edit]
+	before_filter :correct_user,   :only => [:edit, :update]
 
 	def register
 		@user = User.new
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update_attributes(params[:user])
 			sign_in @user
 			flash.now[:success] = "Account Settings Updated"
