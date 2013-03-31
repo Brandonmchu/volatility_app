d5b38d94061c19ba1ad04a70313f64ab5dc0a9ab
Brandon Chu <brandon@tunezy.com>
2013-03-31 12:16:48 -0400

    makes sign in page not use application layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
+  layout :conditional_layout, :only => :home
 
-	
+def home
+end
+
+def conditional_layout
+  if current_user.nil?
+    return 'landing'
+  end
+end
+
 end
